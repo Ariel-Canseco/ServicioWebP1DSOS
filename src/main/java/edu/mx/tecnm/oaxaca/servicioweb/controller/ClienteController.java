cada62d655323ff7eaa772fc664a404d4238812e
Ariel-Canseco <18161156@itoaxaca.edu.mx>
2022-03-10 17:05:38 -0600

    delete alfa

--- src/main/java/edu/mx/tecnm/oaxaca/servicioweb/controller/ClienteController.java
+++ src/main/java/edu/mx/tecnm/oaxaca/servicioweb/controller/ClienteController.java
@@ -1,15 +1,19 @@
 package edu.mx.tecnm.oaxaca.servicioweb.controller;
 
 import edu.mx.tecnm.oaxaca.servicioweb.model.ClienteModel;
+import edu.mx.tecnm.oaxaca.servicioweb.repository.ClienteRepository;
 import edu.mx.tecnm.oaxaca.servicioweb.service.ClienteService;
 import edu.mx.tecnm.oaxaca.servicioweb.utils.CustomResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 /**
  *
  * @author maste
@@ -18,8 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="/api/v1/cliente")
 public class ClienteController {
     
+    private ClienteRepository clienteRepository;
     @Autowired
     private ClienteService clienteService;
+    
+    @Autowired
+    public ClienteController(ClienteRepository clienteRepository){
+    
+        this.clienteRepository = clienteRepository;
+    }
+    
     
     @PostMapping("/")
     public CustomResponse createCliente(@RequestBody ClienteModel clienteModel){
@@ -34,5 +46,11 @@ public class ClienteController {
         CustomResponse customResponse = new CustomResponse();
         customResponse.setDato(clienteService.getClientes());
         return customResponse;
+    }
+    
+    @DeleteMapping("/delete/{id_cliente}")
+    public ResponseEntity<ClienteModel> delete(@PathVariable String id_cliente){
+        clienteRepository.deleteById(Integer.parseInt(id_cliente));
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
